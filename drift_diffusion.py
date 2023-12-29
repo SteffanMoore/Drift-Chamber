c6280bac54b1af713e447441fb433a56ff048967
SteffanMoore <steff.moore@btinternet.com>
2023-12-29 17:45:58 +0000

    Conversion of simulation complete
    The drift chamber simulation has now been completely converted from the old code. Although minor changes might be made, for now it is functional. When run from drift_diffusion.py, a randomly generated cosmic ray is simulated in the chamber and an animation produced. This is then saved as a gif and shown to the user.

--- drift_diffusion.py
+++ drift_diffusion.py
@@ -1,6 +1,21 @@
-def M_matrix(tau, z_cells, x_cells):            #generates the M matrix
+from scipy import sparse
+from scipy.sparse import linalg
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    plot_point_charge()
+
+
+def M_matrix(tau, z_cells, x_cells, grid_spacing):
+    """
+    The charge from the ionisation both drifts towards the anode wires and diffuses into the gas - this can be reprensented by
+    a single matrix acting on a vector: q(n) = M * q(n+1). This M matrix is a largely empty matrix (being a diagonal matrix with
+    a few offset diagonals). Due to this, it is useful to represent it as a sparse matrix to improve performance.
+    """
+
     D, mu, E, h = 0.1, 50, 10**5, grid_spacing
-    alpha, beta, M_dimension = (D*tau)/(h**2), (tau*E)/(2*mu*h), z_cells*x_cells   #n_i and n_j are number of cells
+    alpha, beta, M_dimension = (D * tau) / (h**2), (tau * E) / (2 * mu * h), z_cells * x_cells   #n_i and n_j are number of cells
     diag_centre, diag_alpha_beta, diag_alpha, diagonals = [], [], [], []  #creates lists for diagonal matrices
     
     for i in range(M_dimension):
@@ -16,37 +31,80 @@ def M_matrix(tau, z_cells, x_cells):            #generates the M matrix
     return sparse_matrix
 
 
-print(M_matrix(0.1, 4, 4).toarray())
+def solve_matrix(LU_decomp, q_matrix):
+    """
+    Using the LU decomposition of the sparse M matrix, the charge matrix is solved using the LU factors
+    """
 
+    solved = LU_decomp(q_matrix)
 
-time_limit, timestep = 1E-4, 1E-7
-
-sparse_M_matrix = M_matrix(timestep, z_cells, x_cells)
-solve_M = linalg.factorized(sparse_M_matrix)   #this has been taken out of solve_matrix() to save time when looping
-
-def solve_matrix(M_matrix, q_matrix):   #Performs the LU decomposition
-    solved = solve_M(q_matrix)
     return solved
 
-def charge_diffuse_solve(initial_charge, sparse_M_matrix):    #Finds the charge of the next time step
-    flattened_charge, M = initial_charge.flatten(), sparse_M_matrix
-    solved_charge = solve_matrix(M, flattened_charge)
+
+def charge_diffuse_solve(initial_charge, LU_decomp, z_cells, x_cells):
+    """
+    Finds the charge distribution for the next time step using the initial charge distribution
+    """
+
+    flattened_charge = initial_charge.flatten()
+    solved_charge = solve_matrix(LU_decomp, flattened_charge)
     reshaped = solved_charge.reshape(z_cells, x_cells)
+
     return reshaped
 
-def drift_chamber(initial_charge): #Takes initial charge and runs diffusion/drifting until desired time 
+
+def drift_chamber(initial_charge, time_limit, timestep, LU_decomp, z_cells, x_cells): #Takes initial charge and runs diffusion/drifting until desired time
+    """
+    Takes an initial charge and runs drift-diffusion on it in the specified time-frame
+    """
+
     array_list = []
     array_list.append(initial_charge)
-    for i in range(int(time_limit/timestep)):
-        next_charge = charge_diffuse_solve(initial_charge, solve_M)
+
+    for i in range(int(time_limit / timestep)):
+        next_charge = charge_diffuse_solve(initial_charge, LU_decomp, z_cells, x_cells)
         initial_charge = next_charge
         array_list.append(next_charge)
+
     return array_list
 
 
-def  point_charge():            #creates an array with only one charged cell
+def  point_charge(z_cells, x_cells):
+    """
+    Returns a charge array with a single highly charged cell to represent a point charge
+    """
+
     point_matrix = np.zeros((z_cells, x_cells))
-    point_matrix[int(z_cells/2)][int(x_cells/2)] = 100*1.602E-19
+    point_matrix[int(z_cells/2)][int(x_cells/2)] = 100 * 1.602E-19    # Charge of 100 electrons
+
     return point_matrix
-    
-plt.imshow(drift_chamber(point_charge())[int(time_limit/timestep)])
+
+
+def simulate_chamber(charge_distribution, detector_length, grid_spacing, times):
+
+    x_cells, z_cells = round(detector_length[0] / grid_spacing), round(detector_length[2] / grid_spacing)
+    time_limit, timestep = times[0], times[1]
+
+    sparse_M_matrix = M_matrix(timestep, z_cells, x_cells, grid_spacing)
+
+    # Performs LU decomposition of matrix
+    M_LU_decomp = linalg.factorized(sparse_M_matrix)
+    chamber_simulation = drift_chamber(charge_distribution, time_limit, timestep, M_LU_decomp, z_cells, x_cells)
+
+    return chamber_simulation
+
+
+def plot_point_charge():
+
+    detector_length = (0.5, 1, 0.3)
+    grid_spacing = 0.001
+    x_cells, z_cells = round(detector_length[0] / grid_spacing), round(detector_length[2] / grid_spacing)
+    time_limit, timestep = 1E-4, 1E-7
+
+    simulation = simulate_chamber(point_charge(z_cells, x_cells), (detector_length[0], detector_length[1], detector_length[2]), grid_spacing, (time_limit, timestep))
+    plt.imshow(simulation[int(time_limit / timestep)])
+    plt.show()
+
+
+if __name__ =="__main__":
+    main()
---
+++ simulation.py
@@ -0,0 +1,56 @@
+from drift_diffusion import simulate_chamber
+from ionisation import initial_ionisation
+import matplotlib.pyplot as plt
+import matplotlib.animation as animate
+
+def main():
+    run_simulation((0.5, 1, 0.3), 0.001, (1E-5, 1E-7))
+
+
+def run_simulation(detector_length, grid_spacing, times):
+    """
+    Takes detector length in meters as (x_length, y_length, z_length), grid spacing and times as (time_limit, timestep)
+    and performs a simulation of the detection chamber for a randomly generated cosmic ray. This is then plotted as an animation
+    and saved as drift_diffusion.gif and shown to the user.
+    """
+
+    initial_charge_distribution = initial_ionisation(detector_length, grid_spacing)
+    array_list = simulate_chamber(initial_charge_distribution, detector_length, grid_spacing, times)
+    plot_and_save_gif(array_list, times[0], times[1])
+
+
+def plot_and_save_gif(array_list, time_limit, timestep):
+    """
+    Animates a list of arrays provided as an argument.
+    """
+
+    figure = plt.figure()
+    charge = plt.imshow(array_list[0], animated = True)
+    global frame
+    frame = 0
+
+
+    def next_frame(*args):
+        """
+        Returns the next frame in the animation for FuncAnimation.
+        """
+
+        global frame
+        if frame < int(time_limit/timestep):
+            frame += 1
+        else:
+            frame = 0
+        charge.set_array(array_list[frame])
+
+        return charge
+
+
+    animation = animate.FuncAnimation(figure, next_frame, frames = array_list, interval = 10)
+    writergif = animate.PillowWriter(fps = 30) 
+    animation.save('drift_diffusion.gif', writer = writergif)
+    plt.show() 
+    plt.close()
+
+
+if __name__ == "__main__":
+    main()
